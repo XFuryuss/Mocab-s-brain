8bb27b87e1d96460c1cb8228949fc358270fb609
XFuryus <53180355+XFuryuss@users.noreply.github.com>
2025-04-04 22:08:46 +0200

    Bug fixes reported by Mocab, function re-written.

--- main.c
+++ main.c
@@ -2,43 +2,47 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
+void clearInputBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
+}
 
+int main() {
     // UI displays the current number of active brain cells in Mocab's brain.
     printf("Mocab's currently usning '0' brincels.\n");
-
-    // UI displays if the user wants to turn on Mocab's brain.
-    char brainStatus[60];
+    char brainStatus[10];
     printf("\nDo you want to turn on Mocab's brain? Allowed commands: y/n :D\n");
     printf("-> ");
-    scanf("%59s", brainStatus);
-    while (getchar() != '\n');
+    scanf("%9s", brainStatus);
+    clearInputBuffer(); // Clear any leftover input
 
-    // Function that manages the brain.
-    if (strcmp(brainStatus, "y") == 0 || strcmp(brainStatus, "Y") == 0 || 
-        strcmp(brainStatus, "Yes") == 0 || strcmp(brainStatus, "yes") == 0) {
+    // Function that manages Mocab's brain
+    if (strcmp(brainStatus, "y") == 0 || strcmp(brainStatus, "Y") == 0) {
         int braincells;
-        printf("How many brincels do you wanna turn on?\n");
+        printf("\nHow many brincels to activate?\n");
         printf("-> ");
-        scanf("%d", &braincells);
 
-        switch(braincells){
-            case 0:
-            printf("\nI see, you like your Mocab vanilla :3\n");
-            break;
-            case 1:
-            printf("\nMocab's currently usning '%d' brincels.\n", braincells);
-            printf("Mocab: Quack!\n");
-            break;
-            default:
-            printf("\nError: Woah slow down! Mocab can't stand more than one brincels at the time. >:(\n"); // Error in case the user tries to turn on more than one brain cell.
-            break;
+        if (scanf("%d", &braincells) != 1) {
+            printf("\nError: I don't recognize that command. :(\n");
+            clearInputBuffer(); // Discard the invalid input
         }
-    } else if (strcmp(brainStatus, "n") == 0 || strcmp(brainStatus, "N") == 0 || 
-               strcmp(brainStatus, "No") == 0 || strcmp(brainStatus, "no") == 0) {
-        printf("\nI see, you like your Mocab vanilla :3\n");
-    } else {
-        printf("Error: What are you doing?????? I don't understand your command. :(\n"); // Erorr in case the user doesn't enter a valid command.
+        else {
+            if (braincells <= 0) {
+                printf("\nWhat are you doing!? He already has '0' brincels pls. :(\n");
+            }
+            else if (braincells == 1) {
+                printf("\nMocab's currently usning '1' brincel.\nMocab: Quack!\n");
+            }
+            else {
+                printf("\nSori Mocab doesn't support using more than '1' brincel. ;(\n");
+            }
+        }
+    }
+    else if (strcmp(brainStatus, "n") == 0 || strcmp(brainStatus, "N") == 0) {
+        printf("\nI see you like your Mocab vanilla. :)\n");
+    }
+    else {
+        printf("\nError: I don't recognize that command. :(\n");
     }
 
     return 0;
